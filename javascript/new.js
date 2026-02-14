340cb5a02daf4dd9f4e40f584df31dd31537843f
kaviya-234 <kaviyasulochana234@gmail.com>
2026-02-14 19:09:06 +0530

    Initial commit

---
+++ javascript/new.js
@@ -0,0 +1,8 @@
+let type = new Typed(".typing-text", {
+  strings: ["Full Stack developer..🏄‍♀️", "Web Developer..😇"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backdelay: 500,
+  loop: true,                                               
+});
+
